5367aa2433332d791a77a9d2af86aad9a03e5522
inventionpro <109528211+inventionpro@users.noreply.github.com>
2026-01-29 23:01:34 +0100

    Star ratings

--- media/page/bmselect.js
+++ media/page/bmselect.js
@@ -46,11 +46,12 @@ ${t.map(map=>`<span class="diff" style="--bg:${difficultySpectrumBG(map.difficul
 </div>
 <div class="maps" data-parent="${set.id}" style="--count:${set.beatmaps[window.mode].length}">
   ${set.beatmaps[window.mode].map(map=>`<div class="map" style="--tx:${difficultySpectrumTX(map.difficulty).hex()};--bg:${difficultySpectrumBG(map.difficulty).hex()}">
-  <svg width="15" height="15" viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><rect x="6" y="6" width="116" height="116" rx="58" fill="none" stroke-width="12"/><rect x="23" y="23" width="82" height="82" rx="41"/></svg>
+  <svg width="14" height="14" viewBox="0 0 128 128" xmlns="http://www.w3.org/2000/svg" aria-hidden="true"><rect x="6" y="6" width="116" height="116" rx="58" fill="none" stroke-width="12"/><rect x="23" y="23" width="82" height="82" rx="41"/></svg>
   <div>
     <span>${map.version}</span>
     <div class="stars">
-      <span>${map.difficulty.toFixed(2)}</span>
+      <span><svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" viewBox="0 0 256 256"><path d="M120.392 13.4164C122.786 6.04591 133.214 6.04592 135.608 13.4164L159.881 88.1196C160.952 91.4158 164.024 93.6474 167.489 93.6474H246.037C253.787 93.6474 257.009 103.564 250.739 108.12L187.193 154.289C184.389 156.326 183.216 159.937 184.287 163.233L208.559 237.936C210.954 245.307 202.518 251.436 196.249 246.88L132.702 200.711C129.898 198.674 126.102 198.674 123.298 200.711L59.7514 246.88C53.4817 251.436 45.0458 245.307 47.4407 237.936L71.7132 163.233C72.7842 159.937 71.6109 156.326 68.807 154.289L5.2607 108.12C-1.00901 103.564 2.2132 93.6474 9.96299 93.6474H88.5106C91.9764 93.6474 95.048 91.4158 96.119 88.1196L120.392 13.4164Z"/></svg> ${map.difficulty.toFixed(2)}</span>
+      ${Array.from({ length: Math.max(Math.ceil(map.difficulty),10) },(_,i)=>`<svg xmlns="http://www.w3.org/2000/svg" width="14" height="${Math.max(Math.min(map.difficulty-i,1),0.2)*14}" viewBox="0 0 256 256"><path d="M120.392 13.4164C122.786 6.04591 133.214 6.04592 135.608 13.4164L159.881 88.1196C160.952 91.4158 164.024 93.6474 167.489 93.6474H246.037C253.787 93.6474 257.009 103.564 250.739 108.12L187.193 154.289C184.389 156.326 183.216 159.937 184.287 163.233L208.559 237.936C210.954 245.307 202.518 251.436 196.249 246.88L132.702 200.711C129.898 198.674 126.102 198.674 123.298 200.711L59.7514 246.88C53.4817 251.436 45.0458 245.307 47.4407 237.936L71.7132 163.233C72.7842 159.937 71.6109 156.326 68.807 154.289L5.2607 108.12C-1.00901 103.564 2.2132 93.6474 9.96299 93.6474H88.5106C91.9764 93.6474 95.048 91.4158 96.119 88.1196L120.392 13.4164Z"/></svg>`).join('')}
     </div>
   </div>
 </div>`).join('')}
