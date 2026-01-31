609230625f8a8e433fbaf7385bf7ef1e5d061461
inventionpro <109528211+inventionpro@users.noreply.github.com>
2026-01-31 23:03:08 +0100

    Parser improvements

--- media/util/parser.js
+++ media/util/parser.js
@@ -1,7 +1,13 @@
 function osuSectionKeyPair(content, separator) {
   return Object.fromEntries(content
     .split('\n')
-    .map(line=>line.split(separator)));
+    .map(line=>{
+      let split = line.split(separator);
+      return [split[0],split.slice(1).join(separator)];
+    }));
+}
+function osuStripQuotes(txt) {
+  return txt.replace(/^"(.+?)"$/,'$1');
 }
 const OsuEventTextTypeNumbers = {
   Background: 0,
@@ -28,7 +34,7 @@ function parseOsu(contents) {
   contents = (contents+'\n').replaceAll(/^\/\/.*?$\n/gm,'');
 
   let sections = {};
-  Array.from(contents.matchAll(/\[([a-zA-Z]+?)\]\n((?:.+?\n)+)(?:\n|$)/g))
+  Array.from(contents.matchAll(/\[([a-zA-Z ]+?)\]\n((?:.+?\n)*)(?:\n|$)/g))
     .forEach(sec=>sections[sec[1]]=sec[2]);
 
   // Why does every section use a different content type
@@ -36,7 +42,7 @@ function parseOsu(contents) {
   //sections.Editor = osuSectionKeyPair(sections.Editor, ': '); No editor for now
   sections.Metadata = osuSectionKeyPair(sections.Metadata, ':');
   sections.Difficulty = osuSectionKeyPair(sections.Difficulty, ':');
-  sections.Colours = osuSectionKeyPair(sections.Colours??OsuDefaultColoursSection, ' : ');
+  sections.Colours = osuSectionKeyPair(sections.Colours??OsuDefaultColoursSection, /\s*:\s*/);
 
   sections.Events = sections.Events.split('\n').filter(l=>l.length);
   sections.TimingPoints = sections.TimingPoints.split('\n').filter(l=>l.length);
@@ -53,7 +59,7 @@ function parseOsu(contents) {
   bm.epilepsy = sections.General.EpilepsyWarning==='1';
   bm.maniaSpecialStyle = sections.General.SpecialStyle==='1'; // Adds extra column N+1
 
-  bm.audioFile = sections.General.AudioFilename;
+  bm.audioFile = osuStripQuotes(sections.General.AudioFilename);
   bm.audioDelay = Number(sections.General.AudioLeadIn)||0;
   bm.audioPreview = Number(sections.General.PreviewTime)||-1;
   bm.sampleSet = sections.General.SampleSet??'Normal';
@@ -77,7 +83,7 @@ function parseOsu(contents) {
     switch(type) {
       case 0: // BG
       case 1: // Video
-        extra.file = data[2].replace(/^"(.+?)"$/,'$1');
+        extra.file = osuStripQuotes(data[2]);
         extra.x = Number(data[3])||0;
         extra.y = Number(data[4])||0;
         break;
@@ -86,40 +92,63 @@ function parseOsu(contents) {
         break;
       case 4: // Sound
         extra.layer = Number(data[2]);
-        extra.file = data[3];
+        extra.file = osuStripQuotes(data[3]);
         extra.volume = Number(data[4]);
         break;
       // TODO: Storyboards https://osu.ppy.sh/wiki/en/Storyboard/Scripting
     }
     bm.events.push({ type, start: Number(data[1]), extra });
   });
-  bm.events.sort((a,b)=>a.start-b.start);
+  bm.events.sort((a,b)=>a.start===b.start?a.type-b.type:a.start-b.start);
 
-  // TODO: Timing points, what even are they https://osu.ppy.sh/wiki/en/Client/File_formats/osu_%28file_format%29#timing-points
+  bm.timingPoints = sections.TimingPoints.map(tp=>{
+    let parts = tp.split(',');
+    let effects = Number(parts[7]);
+    return {
+      time: Number(parts[0]),
+      beatLength: Number(parts[1]),
+      meter: Number(parts[2]),
+      sampleSet: ['default','normal','soft','drum'][Number(parts[3])]??'default',
+      sampleIndex: Number(parts[4]),
+      volume: Number(parts[5]),
+      inherited: parts[6]==='0',
+      effects: {
+        kiai: (effects&1)===1,
+        skipFirst: (effects&8)===1
+      }
+    };
+  })
+  .toSorted((a,b)=>a.time-b.time);
 
   bm.colors = {
     sliderBorder: sections.Colours.SliderBorder||'255,255,255',
     sliderTrack: sections.Colours.SliderTrackOverride, // Missing = combo color
     combo: Object.entries(sections.Colours)
       .filter(c=>c[0].startsWith('Combo'))
-      .toSorted((a,b)=>Number(b[0].slice(5))-Number(a[0].slice(5)))
+      .toSorted((a,b)=>Number(a[0].slice(5))-Number(b[0].slice(5)))
       .map(c=>c[1])
   };
 
   bm.objects = sections.HitObjects
     .map(ho=>ho.split(','))
-    .toSorted((a,b)=>Number(b[2])-Number(a[2]))
+    .toSorted((a,b)=>Number(a[2])-Number(b[2]))
     .map(ho=>{
       let typeb = Number(ho[3]);
       let type = typeb&2?'slider':(typeb&8?'spinner':(typeb&128?'hold':'circle'));
-      let hitsound = ho[4]&2?'whistle':(ho[4]&4?'finish':(ho[4]&8?'clap':'normal'));
+      let hitsound = {
+        normal: (ho[4]&1)===1,
+        whistle: (ho[4]&2)===1,
+        finish: (ho[4]&4)===1,
+        clap: (ho[4]&8)===1
+      }
       let extra = {};
       switch(type) {
         case 'slider':
           extra.type = ho[5].split('|')[0];
-          extra.points = ho[5].split('|').slice(1).map(p=>p.split(':')),
+          extra.points = ho[5].split('|').slice(1).map(p=>p.split(':').map(pp=>Number(pp))),
           extra.slides = Number(ho[6]);
           extra.length = Number(ho[7]);
+          // TODO: Edge sounds and sets
           //extra.edgeSounds = ho[8];
           //extra.edgeSets = ho[9];
           break;
