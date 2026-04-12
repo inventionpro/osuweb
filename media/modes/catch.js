469b96fcce5bc504907c3fc97cc6123d74b1e525
inventionpro <109528211+inventionpro@users.noreply.github.com>
2026-04-12 23:09:00 +0200

    Catch better cutting & draw line

--- media/modes/catch.js
+++ media/modes/catch.js
@@ -1,10 +1,18 @@
 window.modeHandelers[2] = (ctx, osu, time)=>{
-  let combo = 0;
   let line = window.gameplayPixelToPos(0, 384)[1];
+
+  PFCTX.fillStyle = `#fff4`;
+  ctx.fillRect(0, line-3, window.innerWidth, 6);
+
+  // Bumper 30x10
+  // Padle 245x26
+
+  let combo = 0;
   osu.objects.forEach(obj=>{
     if (obj.newCombo) combo = (combo+1+obj.comboSkip)%osu.colors.combo.length;
 
-    if (time-obj.time>10) return;
+    if (obj.time-time>window.innerHeight) return;
+    if (time-obj.time>window.innerHeight-line) return;
 
     PFCTX.fillStyle = `rgb(${osu.colors.combo[combo]})`;
     ctx.fillRect(window.gameplayPixelToPos(obj.x, 0)[0], time-obj.time+line, 20, 20);
