f2b158b4880611a75c19068f295da625419bbd3f
inventionpro <109528211+inventionpro@users.noreply.github.com>
2026-05-28 23:35:59 +0200

    Taiko yellow swells

--- media/modes/taiko.js
+++ media/modes/taiko.js
@@ -1,30 +1,40 @@
 window.modeHandelers[1] = (ctx, osu, time, delta)=>{
   let section = window.gameToScreenPixel(384/3);
 
+  if (!window.gameplayData.swellGradient) {
+    window.gameplayData.swellGradient = ctx.createLinearGradient(0, 0, 0, 100);
+    window.gameplayData.swellGradient.addColorStop(0, '#f0c900');
+    window.gameplayData.swellGradient.addColorStop(1, '#a78b00');
+    window.gameplayData.sliderGradient = ctx.createLinearGradient(0, 0, 0, 100);
+    window.gameplayData.sliderGradient.addColorStop(0, '#f1a100');
+    window.gameplayData.sliderGradient.addColorStop(1, '#a76f00');
+  }
+
   // Track
   ctx.fillStyle = '#000c';
   ctx.fillRect(0, section, window.innerWidth, section);
 
   // Notes
   osu.objects.forEach(obj=>{
-
     if (obj.time-time>2000) return;
     if (time-obj.time>100) return;
 
     let radius = section/(obj.hitsound.finish?3:4);
-    let color = obj.hitsound.whistle||obj.hitsound.clap?'#009fee':'#ee0000';
+    let color = obj.type==='spinner'?window.gameplayData.swellGradient:(obj.hitsound.whistle||obj.hitsound.clap?'#009fee':'#ee0000');
     let y = section*1.5;
 
+    ctx.fillStyle = color;
     ctx.strokeStyle = color;
     ctx.lineWidth = 7*(obj.hitsound.finish+1);
     ctx.beginPath();
     ctx.ellipse(obj.time-time, y, radius, radius, 0, 0, Math.PI*2);
     ctx.stroke();
 
-    ctx.fillStyle = color+'cc';
+    ctx.globalAlpha = 0.8;
     ctx.beginPath();
     ctx.ellipse(obj.time-time, y, radius, radius, 0, 0, Math.PI*2);
     ctx.fill();
+    ctx.globalAlpha = 1;
 
     radius = radius/2;
     ctx.fillStyle = '#000';
